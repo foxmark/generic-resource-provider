8cce04c32b7c0ffa174a1ad6cb40a29d831915b5
Foxmark <pawelpasich@yahoo.co.uk>
2026-04-14 23:13:56 +0100

    implementing generic resource processor

--- src/State/Processor/GenericDtoProcessor.php
+++ src/State/Processor/GenericDtoProcessor.php
@@ -2,21 +2,44 @@
 
 namespace App\State\Processor;
 
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Post;
 use ApiPlatform\State\ProcessorInterface;
 use App\Mapper\MapManager;
-use stdClass;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class GenericDtoProcessor implements ProcessorInterface
 {
-    private $mapManager;
-
-    public function __construct(MapManager $mapManager) {
-        $this->mapManager = $mapManager;
-    }
+    public function __construct(
+        private readonly MapManager $mapManager,
+        private readonly EntityManagerInterface $em,
+    ) {}
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): ?object
     {
-        return new stdClass();
+        $mapper     = $this->mapManager->getMapper($operation->getClass());
+        $repository = $this->em->getRepository($mapper->getSupportedEntityClass());
+
+        if ($operation instanceof Delete) {
+            $entity = $repository->find($uriVariables['id']);
+            if ($entity !== null) {
+                $this->em->remove($entity);
+                $this->em->flush();
+            }
+
+            return null;
+        }
+
+        $existing = ($operation instanceof Post)
+            ? null
+            : $repository->find($uriVariables['id']);
+
+        $entity = $mapper->toEntity($data, $existing);
+
+        $this->em->persist($entity);
+        $this->em->flush();
+
+        return $mapper->toResource($entity);
     }
 }
