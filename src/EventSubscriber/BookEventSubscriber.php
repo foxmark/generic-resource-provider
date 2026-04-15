23ed6bd986b5283cf64090c8f2327bfefc2702ff
Foxmark <pawelpasich@yahoo.co.uk>
2026-04-16 00:17:47 +0100

    monolog config

--- src/EventSubscriber/BookEventSubscriber.php
+++ src/EventSubscriber/BookEventSubscriber.php
@@ -4,34 +4,43 @@ namespace App\EventSubscriber;
 
 use App\Event\BookChangeEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class BookEventSubscriber implements EventSubscriberInterface
 {
-    private LoggerInterface $logger;
-
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
+    public function __construct(
+        #[Target('doctrineEntityLogger')]
+        private readonly LoggerInterface $logger,
+    ) {}
 
     public static function getSubscribedEvents(): array
     {
         return [
             BookChangeEvent::CREATED => 'onBookCreated',
-            BookChangeEvent::UPDATED => 'onBookUpdated'
+            BookChangeEvent::UPDATED => 'onBookUpdated',
         ];
     }
 
-    public function onBookCreated(BookChangeEvent $event) 
+    public function onBookCreated(BookChangeEvent $event): void
     {
         $book = $event->getBook();
-        $this->logger->info('Book created callback for ' . $book->getIsbn());
+        $this->logger->info('Book created', [
+            'entity_class' => $book::class,
+            'entity_id'    => $book->getId(),
+            'isbn'         => $book->getIsbn(),
+            'title'        => $book->getTitle(),
+        ]);
     }
 
-    public function onBookUpdated(BookChangeEvent $event) 
+    public function onBookUpdated(BookChangeEvent $event): void
     {
         $book = $event->getBook();
-        $this->logger->info('Book updated callback for ' . $book->getIsbn());
+        $this->logger->info('Book updated', [
+            'entity_class' => $book::class,
+            'entity_id'    => $book->getId(),
+            'isbn'         => $book->getIsbn(),
+            'title'        => $book->getTitle(),
+        ]);
     }
 }
